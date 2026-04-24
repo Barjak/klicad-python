33b7dc252f8d3010197003e87fd587f8ca841fda
Jon Evans <jon@craftyjon.com>
2026-04-23 22:40:24 -0400

    Add Item.clone

--- kipy/wrapper.py
+++ kipy/wrapper.py
@@ -21,17 +21,27 @@
 from abc import ABC, abstractmethod
 from typing import Optional
 
+import sys
+
+if sys.version_info >= (3, 11):
+    from typing import Self
+else:
+    from typing_extensions import Self
+
 from google.protobuf.message import Message
 from kipy.proto.common.types.base_types_pb2 import KIID
 
+
 class Wrapper(ABC):
-    def __init__(self, proto: Optional[Message] = None, proto_ref: Optional[Message] = None):
+    def __init__(
+        self, proto: Optional[Message] = None, proto_ref: Optional[Message] = None
+    ):
         pass
 
     @property
     def proto(self):
         self._pack()
-        return self.__dict__['_proto']
+        return self.__dict__["_proto"]
 
     def _pack(self):
         """Used in some cases to ensure the internal proto state matches the Python
@@ -40,8 +50,19 @@ class Wrapper(ABC):
         """
         pass
 
+
 class Item(Wrapper):
     @property
     @abstractmethod
     def id(self) -> KIID:
         return KIID()
+
+    def clone(self) -> Self:
+        """Creates a copy of this item with the ID field cleared, so that it can be passed to
+        create_items where KiCad will generate a new unique ID for it
+
+        .. versionadded:: 0.8.0"""
+        new_proto = self.proto.__class__()
+        new_proto.CopyFrom(self.proto)
+        new_proto.ClearField("id")
+        return self.__class__(proto=new_proto)
